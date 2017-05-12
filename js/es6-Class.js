aff55214474196b5900134dea3fb441a77926799
walkthecat <58568819@qq.com>
2017-05-12 17:31:46 +0800

    update

--- js/es6-Class.js
+++ js/es6-Class.js
@@ -1,14 +1,14 @@
-class Test {
-    constructor(a, b) {
-        this.a = a;
-        this.b = b;
-    }
+((doc) => {
+    class Test {
+        constructor(a, b) {
+            this.a = a;
+            this.b = b;
+        }
 
-    toString() {
-        return this.a + ',' + this.b;
+        toString() {
+            return this.a + ',' + this.b;
+        }
     }
-}
-
-(() => {
-    console.log((new Test(1, 2)).toString());
-})()
+    // console.log((new Test(1, 2)).toString());
+    doc.innerHTML = (new Test(Math.round(Math.random().toFixed(2) * 100), Math.round(Math.random().toFixed(2) * 100))).toString();
+})(window._presult)
--- js/loadFile.js
+++ js/loadFile.js
@@ -1,24 +1,32 @@
-let jsfiles = ['es6-Class.js'];
-let sel = document.getElementById('jsexp');
-let btn = document.getElementById('btnExecute')
-for (let file of jsfiles) {
-    let opt = document.createElement('option');
-    opt.value = file;
-    opt.text = file;
-    sel.appendChild(opt);
-}
+var _presult = document.getElementById('result');
+(() => {
+    let jsfiles = ['es6-Class.js'];
+    let sel = document.getElementById('jsexp');
+    let btn = document.getElementById('btnExecute');
+    for (let file of jsfiles) {
+        let opt = document.createElement('option');
+        opt.value = file;
+        opt.text = file;
+        sel.appendChild(opt);
+    }
 
-sel.addEventListener('change', (e) => {
-    console.log(e);
-}, false);
-btn.addEventListener('click', () => {
-    loadScript('js/' + sel.value);
-}, false)
+    sel.addEventListener('change', (e) => {
+        console.log(e);
+    }, false);
+    btn.addEventListener('click', () => {
+        loadScript('js/' + sel.value);
+    }, false)
 
-// 动态加载js脚本文件
-function loadScript(url) {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
-    document.body.appendChild(script);
-}
+    // 动态加载js脚本文件
+    function loadScript(url) {
+        let isLoaded = document.querySelector('script[src="' + url + '"]');
+        if (!isLoaded) {
+            let script = document.createElement('script');
+            script.src = url;
+            document.body.appendChild(script);
+        } else {
+            document.body.removeChild(isLoaded);
+            loadScript(url);
+        }
+    }
+})();
