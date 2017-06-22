c04dd99ec50360d94323bb0d1cfe038cfa4c6132
walkthecat <58568819@qq.com>
2017-06-22 14:13:26 +0800

    增加图片压缩功能

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 // Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
 gulp.task('sass', function() {
     gulp.src('css/*.scss')
@@ -12,7 +14,28 @@ gulp.task('sass', function() {
 
 gulp.task('default', ['sass'], function() {
     gulp.watch('css/*.scss', ['sass']);
-})
+});
+
+gulp.task('losslessImagemin', function () {
+    gulp.src('img/*.{png,jpg,gif,ico}')
+        .pipe(imagemin({
+            optimizationLevel: 5, // 类型：Number  默认：3  取值范围：0-7（优化等级）
+            progressive: false, // 类型：Boolean 默认：false 无损压缩jpg图片
+            interlaced: true, // 类型：Boolean 默认：false 隔行扫描gif进行渲染
+            multipass: true // 类型：Boolean 默认：false 多次优化svg直到完全优化
+        }))
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('deepImagemin', function () {
+    gulp.src('img/*.{png,jpg,gif,ico}')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}], // 不要移除svg的viewbox属性
+            use: [pngquant()] // 使用pngquant深度压缩png图片的imagemin插件
+        }))
+        .pipe(gulp.dest('dist/img'));
+});
 
 // Less configuration
 // var gulp = require('gulp');
