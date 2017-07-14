2f5862068e18d64054d03faddbac95d5bd5e61e2
walkthecat <58568819@qq.com>
2017-07-14 09:25:35 +0800

    增加gulp-postcss，autoprefixer

--- AutomateCompilation/ReadMe.md
+++ AutomateCompilation/ReadMe.md
@@ -1,3 +1,6 @@
 ## 安装命令
     npm install -g gulp
-    npm install gulp gulp-sass gulp-less
+    npm install gulp gulp-sass gulp-less gulp-postcss
+
+## 执行命令
+    gulp xx
--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,21 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
+var postcss = require('gulp-postcss');
+//var autoprefixer = require('autoprefixer');
+//var cssnext = require('postcss-cssnext');
+//var shortcss = require('postcss-short');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     gulp.src('css/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest(function(f) {
+        .pipe(gulp.dest(function (f) {
             return f.base;
         }))
 });
 
-gulp.task('default', ['sass'], function() {
+gulp.task('default', ['sass'], function () {
     gulp.watch('css/*.scss', ['sass']);
 });
 
@@ -31,12 +36,22 @@ gulp.task('deepImagemin', function () {
     gulp.src('img/*.{png,jpg,gif,ico}')
         .pipe(imagemin({
             progressive: true,
-            svgoPlugins: [{removeViewBox: false}], // 不要移除svg的viewbox属性
+            svgoPlugins: [{
+                removeViewBox: false
+            }], // 不要移除svg的viewbox属性
             use: [pngquant()] // 使用pngquant深度压缩png图片的imagemin插件
         }))
         .pipe(gulp.dest('dist/img'));
 });
 
+// 用到press,autoprefixer,gulp-postcss,gulp-sourcemaps
+gulp.task('css', function () {
+    return gulp.src('css/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([require('precss'), require('autoprefixer')]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/css'));
+});
 // Less configuration
 // var gulp = require('gulp');
 // var less = require('gulp-less');
